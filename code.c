4f35b2da49e9406559be74e5cb58b65b78671a16
vksunidi <112683020+vksunidi@users.noreply.github.com>
2022-09-02 19:00:34 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+void main
+{
+int low,high,medium;
+int count;
+printf("enter the number of count of batteries");
+scanf("%d",&count);
+if(count<400)
+{
+printf("low");
+}
+else if((count>400) && (count<919))
+{
+printf("medium");
+}
+else if(count>920)
+{
+printf("High");
+}
+else
+{
+printf("invalid input enter the correct");
+}
